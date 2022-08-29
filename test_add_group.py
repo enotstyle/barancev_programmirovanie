1a60193baa99cb3c88b971858b6e7b433a2222bf
Ivan <vanek-te@mail.ru>
2022-08-29 18:53:08 +0700

    add lesson 6

--- test_add_group.py
+++ test_add_group.py
@@ -4,6 +4,7 @@ import pytest
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from barancev_programmirovanie.group import Group
 
 
 def is_alert_present(wd):
@@ -25,19 +26,19 @@ class test_add_group(unittest.TestCase):
     def return_to_groups_page(self, wd):
         wd.find_element(By.LINK_TEXT, "group page").click()
 
-    def create_group(self, wd, name, header, footer):
+    def create_group(self, wd, group):
         # init group creation
         wd.find_element(By.NAME, "new").click()
         # fill group form
         wd.find_element(By.NAME, "group_name").click()
         wd.find_element(By.NAME, "group_name").clear()
-        wd.find_element(By.NAME, "group_name").send_keys(name)
+        wd.find_element(By.NAME, "group_name").send_keys(group.name)
         wd.find_element(By.NAME, "group_header").click()
         wd.find_element(By.NAME, "group_header").clear()
-        wd.find_element(By.NAME, "group_header").send_keys(header)
+        wd.find_element(By.NAME, "group_header").send_keys(group.header)
         wd.find_element(By.NAME, "group_footer").click()
         wd.find_element(By.NAME, "group_footer").clear()
-        wd.find_element(By.NAME, "group_footer").send_keys(footer)
+        wd.find_element(By.NAME, "group_footer").send_keys(group.footer)
         # submit group creation
         wd.find_element(By.NAME, "submit").click()
 
@@ -61,7 +62,7 @@ class test_add_group(unittest.TestCase):
         self.open_home_page(wd)
         self.login(wd, username='admin', password='secret')
         self.open_groups_page(wd)
-        self.create_group(wd, name='first', header='second', footer='thirth')
+        self.create_group(wd, Group(name='first', header='second', footer='thirth'))
         self.return_to_groups_page(wd)
         self.logout(wd)
 
@@ -70,7 +71,7 @@ class test_add_group(unittest.TestCase):
         self.open_home_page(wd)
         self.login(wd, username='admin', password='secret')
         self.open_groups_page(wd)
-        self.create_group(wd, name='', header='', footer='')
+        self.create_group(wd, Group(name='', header='', footer=''))
         self.return_to_groups_page(wd)
         self.logout(wd)
 
