90f4c96c6088e9c706d2d353166cf11fb188bbdb
Ivan <vanek-te@mail.ru>
2022-08-31 15:56:41 +0700

    add lesson 12

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,15 +3,11 @@ import time
 
 
 def test_add_group(app):
-    app.session.login(username='admin', password='secret')
     app.group.create(Group(name='first', header='second', footer='thirth'))
-    app.session.logout()
 
 
 def test_add_empty_group(app):
-    app.session.login(username='admin', password='secret')
     app.group.create(Group(name='', header='', footer=''))
-    app.session.logout()
 
 
 if __name__ == "__main__":
