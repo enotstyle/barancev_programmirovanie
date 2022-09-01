5fac2c43f28973cc39e49746e9e8eb50dc225155
Ivan <vanek-te@mail.ru>
2022-09-01 17:24:20 +0700

    add lesson 19

--- model/group.py
+++ model/group.py
@@ -1,4 +1,4 @@
-
+from sys import maxsize
 
 class Group:
 
@@ -8,8 +8,14 @@ class Group:
         self.footer = footer
         self.id = id
 
+    def id_or_max(self):
+        if self.id:
+            return int(self.id)
+        else:
+            return maxsize
+
     def __repr__(self):
         return f'{self.id}:{self.name}'
 
     def __eq__(self, other):
-        return self.id == other.id and self.name == other.name
+        return (self.id is None or other.id is None or self.id == other.id) and self.name == other.name
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,21 +1,21 @@
 from barancev_programmirovanie.model.group import Group
-import time
 
 
 def test_add_group(app):
     old_groups = app.group.get_group_list()
-    app.group.create(Group(name='first', header='second', footer='thirth'))
+    group = Group(name='first', header='second', footer='thirth')
+    app.group.create(group)
     new_groups = app.group.get_group_list()
-
     assert len(old_groups) + 1 == len(new_groups)
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+
 
 def test_add_empty_group(app):
     old_groups = app.group.get_group_list()
-    app.group.create(Group(name='', header='', footer=''))
+    group = Group(name='', header='', footer='')
+    app.group.create(group)
     new_groups = app.group.get_group_list()
-
     assert len(old_groups) + 1 == len(new_groups)
-
-
-if __name__ == "__main__":
-    unittest.main()
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
