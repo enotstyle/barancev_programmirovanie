72abb79cecaefa4b9f45928252bfe3f2b821ccba
Ivan <vanek-te@mail.ru>
2022-09-02 14:58:02 +0700

    add lesson 22

--- fixture/application.py
+++ fixture/application.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from barancev_programmirovanie.fixture.session import SessionHelper
 from barancev_programmirovanie.fixture.group import GroupHelper
+from barancev_programmirovanie.fixture.contact import ContactHelper
 
 class Application:
 
@@ -8,6 +9,7 @@ class Application:
         self.wd = webdriver.Firefox()
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
+        self.contact = ContactHelper(self)
 
     def open_home_page(self):
         wd = self.wd
