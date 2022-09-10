ed2ce1ef038018a1189e37b66840ef7122df3a5a
Ivan <vanek-te@mail.ru>
2022-09-10 15:31:42 +0700

    add lesson 31

---
+++ data/groups.py
@@ -0,0 +1,6 @@
+from barancev_programmirovanie.model.group import Group
+
+constant = [
+    Group(name="name1", header="header1", footer="footer1"),
+    Group(name="name2", header="header2", footer='footer2')
+]
